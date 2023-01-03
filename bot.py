8203d8da0a7c8c84b8a84b8c19608601a7cd7610
Aleksei Chabanov <serverqam@gmail.com>
2023-01-03 04:54:00 +0300

    unit 2

--- bot.py
+++ bot.py
@@ -21,52 +21,18 @@ async def command_start(message: types.Message):
 
 @dp.message_handler()
 async def bot_message(message: types.Message):
-    # await bot.send_message(message.from_user.id, message.text)
-    if message.text == 'Продукты':
-        await bot.send_message(message.from_user.id, 'Меню "Продукты"', reply_markup=nav.productsMenu)
-    elif message.text == 'Скачать бесплатно':
-        await bot.send_message(message.from_user.id, 'Меню "Скачать бесплатно"', reply_markup=nav.downloadMenu)
-    elif message.text == 'Контакты':
-        await bot.send_message(message.from_user.id, 'Меню "Контакты"', reply_markup=nav.contactsMenu)
-    elif message.text == 'Главное меню':
+    for key, value in nav.dict_buttons_Contacts.items():
+        if message.text == key:
+            await bot.send_message(message.from_user.id, f'{value}', reply_markup=nav.contactsMenu)
+    for key, value in nav.dict_buttons_Products.items():
+        if message.text == key:
+            await bot.send_message(message.from_user.id, f'{value}', reply_markup=nav.productsMenu)
+    for key, value in nav.dict_buttons_Download.items():
+        if message.text == key:
+            await bot.send_message(message.from_user.id, f'{value}', reply_markup=nav.downloadMenu)
+    if message.text == 'Главное меню':
         await bot.send_message(message.from_user.id, 'Главное меню', reply_markup=nav.mainMenu)
-    elif message.text == 'База кроссов FAPI':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'Каталог  по маркам':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'База фото автозапчастей':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'Штрих-коды автозапчастей':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'База артикулов автозапчастей':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == '1С: Автобизнес':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'API интеграция':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'База кроссов онлайн':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'База автовладельцев ВКонтакте':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'Фото логотипов автомобилей':
-        await bot.send_message(message.from_user.id, 'Потом здесь будет описание базы и способ оплаты',
-                               reply_markup=nav.productsMenu)
-    elif message.text == 'Ссылки для скачивания':
-        await bot.send_message(message.from_user.id, 'Потом здесь будут ссылки на скачивание',
-                               reply_markup=nav.downloadMenu)
-    elif message.text == 'Email':
-        await bot.send_message(message.from_user.id, 'Потом здесь адрес и переход на почту',
-                               reply_markup=nav.contactsMenu)
-    else:
+    if message.text not in nav.ollMenu:
         await message.reply('Выберите пункт меню')
 
 
--- markups.py
+++ markups.py
@@ -3,29 +3,36 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 btnMain = KeyboardButton('Главное меню')
 
 # ---Main Menu---
-btn1 = KeyboardButton('Продукты')
-btn2 = KeyboardButton('Скачать бесплатно')
-btn3 = KeyboardButton('Контакты')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn1, btn2, btn3)
+buttons_Main = ['Продукты', 'Скачать бесплатно', 'Контакты']
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons_Main)
 
 # ---Products Menu---
-btn11 = KeyboardButton('База кроссов FAPI')
-btn12 = KeyboardButton('Каталог  по маркам')
-btn13 = KeyboardButton('База фото автозапчастей')
-btn14 = KeyboardButton('Штрих-коды автозапчастей')
-btn15 = KeyboardButton('База артикулов автозапчастей')
-btn16 = KeyboardButton('1С: Автобизнес')
-btn17 = KeyboardButton('API интеграция')
-btn18 = KeyboardButton('База кроссов онлайн')
-btn19 = KeyboardButton('База автовладельцев ВКонтакте')
-btn110 = KeyboardButton('Фото логотипов автомобилей')
-productsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18,
-                                                             btn19, btn110, btnMain)
+buttons_Products = ['База кроссов FAPI', 'Каталог  по маркам', 'База фото автозапчастей', 'Штрих-коды автозапчастей',
+                    'База артикулов автозапчастей', '1С: Автобизнес', 'API интеграция', 'База кроссов онлайн',
+                    'База автовладельцев ВКонтакте', 'Фото логотипов автомобилей']
+productsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons_Products, btnMain)
+dict_buttons_Products = {'Продукты': 'Вы перешли в меню "Продукты"',
+                         'База кроссов FAPI': 'Потом здесь будет описание базы и способ оплаты',
+                         'Каталог  по маркам': 'Потом здесь будет описание базы и способ оплаты',
+                         'База фото автозапчастей': 'Потом здесь будет описание базы и способ оплаты',
+                         'Штрих-коды автозапчастей': 'Потом здесь будет описание базы и способ оплаты',
+                         'База артикулов автозапчастей': 'Потом здесь будет описание базы и способ оплаты',
+                         '1С: Автобизнес': 'Потом здесь будет описание базы и способ оплаты',
+                         'API интеграция': 'Потом здесь будет описание базы и способ оплаты',
+                         'База кроссов онлайн': 'Потом здесь будет описание базы и способ оплаты',
+                         'База автовладельцев ВКонтакте': 'Потом здесь будет описание базы и способ оплаты',
+                         'Фото логотипов автомобилей': 'Потом здесь будет описание базы и способ оплаты'}
 
 # ---Download Menu---
-btn21 = KeyboardButton('Ссылки для скачивания')
-downloadMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn21, btnMain)
-
+buttons_Download = ['Ссылки для скачивания']
+downloadMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons_Download, btnMain)
+dict_buttons_Download = {'Скачать бесплатно': 'Вы перешли в меню "Скачать бесплатно"',
+                         'Ссылки для скачивания': 'Потом здесь будут ссылки на скачивание'}
 # ---Contacts Menu---
-btn31 = KeyboardButton('Email')
-contactsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn31, btnMain)
+buttons_Contacts = ['Email']
+contactsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons_Contacts, btnMain)
+dict_buttons_Contacts = {'Контакты': 'Вы перешли в меню "Контакты"',
+                         'Email': 'Потом здесь будет адрес и переход на почту'}
+
+ollMenu = buttons_Main + buttons_Contacts + buttons_Download + buttons_Products + ['Главное меню']
+
