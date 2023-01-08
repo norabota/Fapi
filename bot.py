e3d89eefd25c7344532388bf3183004832d84ddf
Aleksei Chabanov <serverqam@gmail.com>
2023-01-08 03:53:17 +0300

    unit 4

--- bot.py
+++ bot.py
@@ -9,29 +9,29 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-
 async def on_startup(_):
     print('Бот успешно запущен!')
-
-
-@dp.message_handler(commands=['start'])
-async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Привет, {0.first_name}!'.format(message.from_user),
-                           reply_markup=nav.mainMenu)
-
 
 with open('data_file.json', "r") as file:
     data = json.load(file)
 
 
+@dp.message_handler(commands=['start'])
+async def command_start(message: types.Message):
+    key = data.get("Главное меню")
+    reply_markup = nav.menu_id(key["markup"])
+    await bot.send_message(message.from_user.id, 'Здравствуйте, {0.first_name}!'.format(message.from_user),
+                           reply_markup=reply_markup)
+
+
 @dp.message_handler()
 async def bot_message(message: types.Message):
-    key = data.get(message.text)
-    ss = key["markup"]
-
-    await bot.send_message(message.from_user.id, f'{key["answer"]}', reply_markup=nav.mainMenu)
     if message.text not in nav.ollMenu:
         await message.reply('Выберите пункт меню...')
+    key = data.get(message.text)
+    reply_markup = nav.menu_id(key["markup"])
+    await bot.send_message(message.from_user.id, f'{key["answer"]}', reply_markup=reply_markup)
+
 
 
 if __name__ == '__main__':
