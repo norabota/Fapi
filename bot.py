6857127fcf5d61905979e2c14459e25cc6987c89
Aleksei Chabanov <serverqam@gmail.com>
2023-01-10 04:34:26 +0300

    unit 5

--- bot.py
+++ bot.py
@@ -2,36 +2,33 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 import config
 import markups as nav
-import json
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
+
 async def on_startup(_):
     print('Бот успешно запущен!')
-
-with open('data_file.json', "r") as file:
-    data = json.load(file)
 
 
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    key = data.get("Главное меню")
-    reply_markup = nav.menu_id(key["markup"])
+    markup_menu = nav.menu_id('start')
     await bot.send_message(message.from_user.id, 'Здравствуйте, {0.first_name}!'.format(message.from_user),
-                           reply_markup=reply_markup)
+                           reply_markup=markup_menu[1])
 
 
 @dp.message_handler()
 async def bot_message(message: types.Message):
-    if message.text not in nav.ollMenu:
+    if message.text  in nav.allMenu:
+        markup_menu = nav.menu_id(message.text)
+        await bot.send_message(message.from_user.id, f'{markup_menu[0]}', reply_markup=markup_menu[1])
+    else:
         await message.reply('Выберите пункт меню...')
-    key = data.get(message.text)
-    reply_markup = nav.menu_id(key["markup"])
-    await bot.send_message(message.from_user.id, f'{key["answer"]}', reply_markup=reply_markup)
-
+        # markup_menu = nav.menu_id(message.text)
+        # await bot.send_message(message.from_user.id, f'{markup_menu[0]}', reply_markup=markup_menu[1])
 
 
 if __name__ == '__main__':
--- markups.py
+++ markups.py
@@ -1,12 +1,30 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup
+import json
 
-ollMenu = ['База кроссов FAPI', 'Каталог  по маркам', 'База фото автозапчастей', 'Штрих-коды автозапчастей',
+with open('data_file.json', "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+allMenu = ['База кроссов FAPI', 'Каталог  по маркам', 'База фото автозапчастей', 'Штрих-коды автозапчастей',
            'База артикулов автозапчастей', '1С: Автобизнес', 'API интеграция', 'База кроссов онлайн',
            'База автовладельцев ВКонтакте', 'Фото логотипов автомобилей', 'Главное меню', 'Продукты',
            'Скачать бесплатно', 'Ссылки для скачивания', 'Email', 'Контакты']
 
 
-def menu_id(list_buttons):
-    return ReplyKeyboardMarkup(resize_keyboard=True).add(*list_buttons)
-
-
+def menu_id(msg):
+    # Проверка входящего сообщения на соответствие значению ключа
+    # Перебор словаря, создание клавиатуры меню
+    list_buttons = []
+    if msg == data["menu"][0]["menuItem"]["description"] or msg == 'start':
+        items_main_menu = data["menu"][0]["menuItem"]["items"]
+        msg_answer = data["menu"][0]["menuItem"]["answer"]
+        for i in items_main_menu:
+            list_buttons.append(i["description"])
+        return [msg_answer, ReplyKeyboardMarkup(resize_keyboard=True).add(*list_buttons)]
+    else:
+        items_msg_menu = data["menu"]
+        for i in items_msg_menu:
+            if i["menuItem"]["description"] == msg:
+                msg_answer = i["menuItem"]["answer"]
+                for key in i["menuItem"]["items"]:
+                    list_buttons.append(key["description"])
+                return [msg_answer, ReplyKeyboardMarkup(resize_keyboard=True).add(*list_buttons)]
