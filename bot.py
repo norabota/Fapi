99c6287b3d2048c86f3ddcb6b47ef77de98c6733
Aleksei Chabanov <serverqam@gmail.com>
2023-02-08 00:11:41 +0300

    Добавлена ф-я  createInlineKeyboardButtons. Правка id в json

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 import config
-import markups as nav
-import inline_markups as inav
+import inline_markups as nav
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,43 +15,23 @@ async def on_startup(_):
 
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    markup_menu = inav.menu_commands('start')
-    print(markup_menu)
-    await bot.send_message(message.from_user.id, '.           Здравствуйте, {0.first_name}!         .'.format(message.from_user),
-                           reply_markup=markup_menu["submenu"])
+    inline_menu = nav.menu_commands('start')
+    print(inline_menu)
+    await bot.send_message(message.from_user.id,
+                           '.           Здравствуйте, {0.first_name}!         .'.format(message.from_user),
+                           reply_markup=inline_menu["submenu"])
 
 
 @dp.callback_query_handler(lambda call: True)
 async def process_callback(callback_query: types.CallbackQuery):
     code = callback_query.data
-    print(6666666,code)
-    inline_callback_menu = inav.menu_inline(callback_query.data)
-    if inline_callback_menu:
-        await bot.send_message(callback_query.from_user.id, f'{inline_callback_menu["answer"]}',
-                           reply_markup=inline_callback_menu["submenu"],
-                           parse_mode="MarkdownV2")
+    print(1, code)
+    inline_menu = nav.menu_inline(callback_query.data)
+    if inline_menu:
+        await bot.send_message(callback_query.from_user.id, f'{inline_menu["answer"]}',
+                               reply_markup=inline_menu["submenu"],
+                               parse_mode="MarkdownV2")
         await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
-
-
-# @dp.message_handler()
-# async def bot_message(message: types.Message):
-#     markup_menu = nav.menu_id(message.text)
-#     inline_links_menu = inav.menu_id_links(message.text)
-#     inline_callback_menu = inav.menu_inline(message.text)
-#     print(99, inline_callback_menu)
-#     print(65, inline_links_menu)
-#     print(67, markup_menu)
-#     if inline_links_menu:
-#         await message.answer("Перейдите по ссылке", reply_markup=inline_links_menu["submenu"], parse_mode="MarkdownV2")
-#     if markup_menu:
-#         await bot.send_message(message.from_user.id, f'{markup_menu["answer"]}', reply_markup=markup_menu["submenu"],
-#                                parse_mode="MarkdownV2")
-#     if inline_callback_menu:
-#         await message.answer(f'{inline_callback_menu["answer"]}', reply_markup=inline_callback_menu["submenu"],
-#                              parse_mode="MarkdownV2")
-#     if markup_menu is False and inline_links_menu is False and inline_callback_menu is False:
-#         await message.reply(
-#             'Выбранный  пункт меню сейчас в разработке, выберите другой пункт меню из предложенной клавиатуры')
 
 
 if __name__ == '__main__':
--- inline_markups.py
+++ inline_markups.py
@@ -15,70 +15,33 @@ except Exception as err:
 
 
 def menu_commands(msg):
-    list_buttons = []
     if msg == 'start':
-        items_main_menu = data["menu"][0]["menuItem"]["menuItems"]
-        msg_answer = data["menu"][0]["menuItem"]["answer"]
-        for key in items_main_menu:
-            if key["menuItem"].get("callback_data", 0):
-                b = InlineKeyboardButton(text=key["menuItem"]["description"],
-                                         callback_data=key["menuItem"]["callback_data"])
-                list_buttons.append(b)
-        if list_buttons:
-            return {"answer": msg_answer, "submenu": InlineKeyboardMarkup(row_width=1).add(*list_buttons)}
-        # for i in items_main_menu:
-        #     list_buttons.append(i["menuItem"]["description"])
-        # return {"answer": msg_answer, "submenu": InlineKeyboardMarkup(row_width=1).add(*list_buttons)}
+        return createInlineKeyboardButtons(data["menu"][0]["menuItem"]["answer"],
+                                           data["menu"][0]["menuItem"]["menuItems"])
 
 
-def menu_id_links(msg, submenu_with_msg=data["menu"]):
-    list_buttons = []
-    for i in submenu_with_msg:
-        if i["menuItem"]["description"] == msg:
-            msg_answer = i["menuItem"]["answer"]
-            if "links" in i["menuItem"]:
-                list_buttons = [InlineKeyboardButton(text=key, url=values) for key, values in
-                                i["menuItem"]["links"].items()]
-                return {"answer": msg_answer, "submenu": InlineKeyboardMarkup(row_width=1).add(*list_buttons)}
+def menu_inline(msg, menuItem=data["menu"]):
+    print(2, menuItem)
+    for item in menuItem:
+        if item["menuItem"]["callback_data"] == msg:
+            return createInlineKeyboardButtons(item["menuItem"]["answer"], item["menuItem"]["menuItems"])
         else:
-            answer_submenu = menu_id_links(msg, i["menuItem"]["menuItems"])
-            if answer_submenu != False:
-                return answer_submenu
-    return False
+            menu = menu_inline(msg, item["menuItem"]["menuItems"])
+            if menu is not None:
+                return menu
+    return None
 
 
-def menu_inline(msg, submenu_with_msg=data["menu"]):
+def createInlineKeyboardButtons(msg_answer, menu_items):
     list_buttons = []
-    print(1, submenu_with_msg)
-    for i in submenu_with_msg:
-        if i["menuItem"]["menuItems"] == []:
-            continue
-        elif i["menuItem"]["callback_data"] == msg:
-            print(2, i)
-            print(3, i["menuItem"].get("menuItems"))
-            msg_answer = i["menuItem"]["answer"]
-            for key in i["menuItem"]["menuItems"]:
-                if key["menuItem"].get("callback_data", 0):
-                    if key["menuItem"]["id"].startswith('link'):
-                        b = InlineKeyboardButton(text=key["menuItem"]["description"],
-                                                 url=key["menuItem"]["callback_data"])
-                        list_buttons.append(b)
-                    else:
-                        b = InlineKeyboardButton(text=key["menuItem"]["description"],
-                                         callback_data=key["menuItem"]["callback_data"])
-                        list_buttons.append(b)
-            if list_buttons:
-                return {"answer": msg_answer, "submenu": InlineKeyboardMarkup(row_width=1).add(*list_buttons)}
+    for item in menu_items:
+        if item["menuItem"]["id"].startswith('link'):
+            button = InlineKeyboardButton(text=item["menuItem"]["description"],
+                                     url=item["menuItem"]["callback_data"])
+            list_buttons.append(button)
         else:
-            answer_submenu = menu_inline(msg, i["menuItem"]["menuItems"])
-            if not answer_submenu == False:
-                return answer_submenu
-    return False
-
-
-# # кнопки из списка
-# l = ['Яблоко', 'Груша']
-# keyboard = InlineKeyboardMarkup(row_width=1)
-# backbutton = InlineKeyboardButton(text="Back", callback_data="MainMenu")
-# button_list = [InlineKeyboardButton(text=x, callback_data=x) for x in l]
-# keyboard.add(*button_list, backbutton)
+            button = InlineKeyboardButton(text=item["menuItem"]["description"],
+                                     callback_data=item["menuItem"]["callback_data"])
+            list_buttons.append(button)
+    if list_buttons:
+        return {"answer": msg_answer, "submenu": InlineKeyboardMarkup(row_width=1).add(*list_buttons)}
